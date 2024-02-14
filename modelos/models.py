e31211b90e3639d25f81f7e0fe917c096c2aabfe
Daniela <dggtn9@gmail.com>
2024-02-14 17:37:16 -0300

    diseno blog

--- modelos/models.py
+++ modelos/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    titulo = models.CharField(max_length=20)
-    cuerpo = models.CharField(max_length=120)
+    titulo = models.CharField(max_length=120)
+    cuerpo = models.CharField(max_length=5000)
 
 class Usuarios(models.Model):
     nombre = models.CharField(max_length=15)
     apellido = models.CharField(max_length=10)
     apodo = models.CharField(max_length=10)
-    pais = models.CharField(max_length=8)
+    pais = models.CharField(max_length=20)
 
 class Comentarios(models.Model):
     autor = models.CharField(max_length=15)
