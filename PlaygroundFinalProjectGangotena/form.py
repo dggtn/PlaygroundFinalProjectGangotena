23989bc42b007583015147fa67ba332a5070b083
Daniela <dggtn9@gmail.com>
2024-02-15 23:42:17 -0300

    usuario

--- PlaygroundFinalProjectGangotena/form.py
+++ PlaygroundFinalProjectGangotena/form.py
@@ -4,9 +4,9 @@ from django.contrib.auth.forms import UserCreationForm,UserModel
 class UserCreationFormCustom(UserCreationForm):
     user = forms.CharField(label="Usuario")
     email = forms.EmailField()
-    password1 = forms.CharField(label="contrasena, widget=forms.PasswordInput")
+    password1 = forms.CharField(label="contrasena:", widget=forms.PasswordInput)
     password2 = forms.CharField(label="repetir contrasena", widget=forms.PasswordInput)
     class Meta:
         model = UserModel
-        fields =  ['username','email','password1','password2']       
+        fields =  ['user','email','password1','password2']       
         help_texts = {k:"" for k in fields}
