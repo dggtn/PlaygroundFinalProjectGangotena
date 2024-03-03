9cda5801c46bd3fecce594f8d7fd9207d4287932
d G <dggtn97@gmail.com>
2024-03-03 12:12:29 -0300

    iamgenes avatares y mensajeria muchos a uno

--- mensajes/models.py
+++ mensajes/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Chat(models.Model):
     fecha_inicio = models.DateTimeField(auto_now_add=True)
-    participante_uno = models.OneToOneField(User, on_delete=models.CASCADE, related_name="participante_uno")
-    participante_dos = models.OneToOneField(User, on_delete=models.CASCADE, related_name="participante_dos")
+    participante_uno = models.ForeignKey(User, on_delete=models.CASCADE, related_name="participante_uno")
+    participante_dos = models.ForeignKey(User, on_delete=models.CASCADE, related_name="participante_dos")
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
